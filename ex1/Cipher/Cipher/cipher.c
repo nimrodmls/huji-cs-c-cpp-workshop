fee2250bf11edd65d8a4243b50428e0c8d8a1cfa
Nimrod Mallis <nimrodmallis@gmail.com>
2023-03-03 19:51:13 +0200

    Implemented file reading, added some docs

--- ex1/Cipher/Cipher/cipher.c
+++ ex1/Cipher/Cipher/cipher.c
@@ -1,9 +1,15 @@
 #include <ctype.h>
 #include "cipher.h"
 
+// Letter count in the english alphabet
 #define LETTER_COUNT 26
 
-char manipulate_character(const char in, int shift)
+/**
+ * Shifts the given character within the English Alphabet
+ * @param in - The character to shift
+ * @param shift - The amount of characters to shift
+ */
+char manipulate_character(const char in, const int shift)
 {
 	int new_char = in;
 	int new_shift = 0;
@@ -41,12 +47,14 @@ void encode (char s[], int k)
 	unsigned long index = 0;
 	for (index = 0; s[index] != 0; index++)
 	{
-		s[index] = (char)manipulate_character(s[index], k);
+		s[index] = manipulate_character(s[index], k);
 	}
 }
 
 // See full documentation in header file
 void decode (char s[], int k)
 {
+	// Encode does the same as decode,
+	// but with "decoding" encryption key (the inverse)
 	encode(s, -k);
 }
--- ex1/Cipher/Cipher/main.c
+++ ex1/Cipher/Cipher/main.c
@@ -5,8 +5,84 @@
 #include "cipher.h"
 #include "tests.h"
 
+#define ENCODE_COMMAND "encode"
+#define DECODE_COMMAND "decode"
+
+void get_file_data(
+	__in const char * file_path, __deref_out char ** out_data)
+{
+	FILE* file_handle = NULL;
+	char* file_data = NULL;
+	unsigned long file_size = 0;
+	size_t read_bytes = 0;
+	
+	if (NULL == file_path || NULL == out_data)
+	{
+		goto lblCleanup;
+	}
+
+	file_handle = fopen(file_path, "r");
+	if (NULL == file_handle)
+	{
+		goto lblCleanup;
+	}
+
+	if (0 != fseek(file_handle, 0, SEEK_END))
+	{
+		goto lblCleanup;
+	}
+
+	file_size = ftell(file_handle);
+	rewind(file_handle);
+
+	// Allocating +1 for the file data in order to make space
+	// for the null-terminator
+	file_data = calloc(file_size + 1, sizeof(char));
+	if (NULL == file_data)
+	{
+		goto lblCleanup;
+	}
+
+	read_bytes = fread(file_data, sizeof(char), file_size, file_handle);
+	if (file_size != read_bytes)
+	{
+		goto lblCleanup;
+	}
+
+	if (ferror(file_handle))
+	{
+		goto lblCleanup;
+	}
+
+	// Transferring ownership over the data
+	*out_data = file_data;
+	file_data = NULL;
+
+lblCleanup:
+	if (NULL != file_handle)
+	{
+		(void)fclose(file_handle);
+	}
+
+	if (NULL != file_data)
+	{
+		free(file_data);
+	}
+}
+
+void encode_decode(char * cmd, 
+				   int shift_count, 
+				   char * in_file, 
+				   char * out_file)
+{
+	
+}
+
 int main (int argc, char *argv[])
 {
+	char* out = NULL;
+	get_file_data("C:\\temp\\test.txt", &out);
+	printf(out);
 	char * str = (char *)calloc(30, 1);
 	char* str2 = (char*)calloc(30, 1);
 	char * a = "Hello, world!\n";
