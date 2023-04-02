8cf7ae78b1cbdfc4471a1c3623751af18554ab0d
Nimrod Mallis <nimrodmallis@gmail.com>
2023-04-02 23:47:48 +0300

    Ground work for all the input mechanism

--- ex2/ex2/main.c
+++ ex2/ex2/main.c
@@ -4,14 +4,249 @@
 #include "sort_bus_lines.h"
 #include "test_bus_lines.h"
 
-BusLine create_line(char* name, int distance, int duration)
-{
-	BusLine line = { 0 };
-	memcpy(&line.name, name, sizeof(line.name));
-	line.distance = distance;
-	line.duration = duration;
-	return line;
+// Constants
+#define DECIMAL_BASE (10)
+#define INPUT_BUFFER_SIZE_BYTES (61)
+#define READ_UNTIL_NEW_LINE ("%[^\n]")
 
+// Prompt strings for the user
+#define LINE_COUNT_PROMPT ("Enter number of lines. Then enter\n")
+#define USAGE_PROMPT ("USAGE: sort_lines [by_name/by_duration/by_distance/test]\n")
+#define ERROR_INVALID_LINE_COUNT ("ERROR: ")
+
+// Constants for command type string identifier
+#define COMMAND_BY_DURATION_STR ("by_duration")
+#define COMMAND_BY_DISTANCE_STR ("by_distance")
+#define COMMAND_BY_NAME_STR ("by_name")
+#define COMMAND_TEST_STR ("test")
+
+//
+typedef enum ProgramStatus
+{
+	PROGRAM_STATUS_SUCCESS = EXIT_SUCCESS,
+	PROGRAM_STATUS_FAILED = EXIT_FAILURE
+
+} ProgramStatus, *pProgramStatus;
+
+//
+typedef enum Arguments
+{
+	ARGUMENT_COMMAND = 1,
+	ARGUMENT_MAX_ARGS
+
+} Arguments, *pArguments;
+
+// 
+typedef enum CommandType
+{
+	COMMAND_INVALID = 0,
+	COMMAND_BY_DURATION,
+	COMMAND_BY_DISTANCE,
+	COMMAND_BY_NAME,
+	COMMAND_TEST
+
+} CommandType, *pCommandType;
+
+//
+typedef ProgramStatus(*InputTester) (
+	char* input, void* user_input);
+
+// Macros
+//
+#define STATUS_FAILED(status) (PROGRAM_STATUS_SUCCESS != status)
+
+//
+#define FREE_MEMORY(ptr) \
+{						 \
+	if (NULL != ptr)	 \
+	{					 \
+		free(ptr);		 \
+	}					 \
+}
+ 
+// Function declarations
+
+/**
+ * Resolving the command type from the given string.
+ * @param in - The input string to resolve.
+ * @param cmd - The output command type, once resolved.
+ * @return Whether the resolution has been successful or not.
+ */
+ProgramStatus resolve_command_type(char* in, pCommandType cmd);
+
+// Fuction definitions
+
+ProgramStatus get_user_input(char* prompt, InputTester tester, void* user_input)
+{
+	ProgramStatus status = PROGRAM_STATUS_FAILED;
+	char input_local[INPUT_BUFFER_SIZE_BYTES] = { 0 };
+
+	if ((NULL == user_input) || (NULL == tester))
+	{
+		return status;
+	}
+
+	do
+	{
+		(void)fprintf(stdout, prompt);
+		if (NULL == fgets(input_local, sizeof(input_local), stdin))
+		{
+			return status;
+		}
+
+		if (EOF == sscanf(input_local, READ_UNTIL_NEW_LINE, input_local))
+		{
+			return status;
+		}
+
+	} while (!STATUS_FAILED(tester(input_local, user_input)));
+
+	status = PROGRAM_STATUS_SUCCESS;
+	return status;
+}
+
+// See documentation at declaration
+ProgramStatus convert_str_to_long(const char* in, long* out)
+{
+	ProgramStatus status = PROGRAM_STATUS_FAILED;
+	long conv = 0;
+	char* result = NULL;
+
+	if (NULL == out)
+	{
+		goto cleanup;
+	}
+
+	errno = 0; // Setting errno to 0 to make sure strstol failed
+	conv = strtol(in, &result, DECIMAL_BASE);
+	// Dealing with all failure cases for conversion - 
+	// if errno is set, or the string has non-numerical chars
+	if (((0 == conv) && (errno != 0)) ||
+		(0 != *result))
+	{
+		goto cleanup;
+	}
+
+	*out = conv;
+	status = PROGRAM_STATUS_SUCCESS;
+
+cleanup:
+	return status;
+}
+
+ProgramStatus bus_line_count_tester(char* input, long* line_count)
+{
+	ProgramStatus status = PROGRAM_STATUS_FAILED;
+	long count = 0;
+
+	if ((NULL == input) || (NULL == line_count))
+	{
+		return status;
+	}
+	
+	status = convert_str_to_long(input, &count);
+	if (STATUS_FAILED(status))
+	{
+		return status;
+	}
+
+	if (count <= 0)
+	{
+		(void)fprintf(stdin, ERROR_INVALID_LINE_COUNT);
+		return status;
+	}
+
+	*line_count = count;
+
+	status = PROGRAM_STATUS_SUCCESS;
+
+	return status;
+}
+
+ProgramStatus get_single_bus_line(BusLine* bus_line)
+{
+	return 0;
+}
+
+ProgramStatus get_bus_line_count(long* count)
+{
+	ProgramStatus status = PROGRAM_STATUS_FAILED;
+	char input[INPUT_BUFFER_SIZE_BYTES] = { 0 };
+
+	if (NULL == count)
+	{
+		return status;
+	}
+
+
+
+
+
+	status = PROGRAM_STATUS_SUCCESS;
+	return status;
+}
+
+ProgramStatus get_bus_lines_input(BusLine** bus_lines)
+{
+	ProgramStatus status = PROGRAM_STATUS_FAILED;
+	BusLine* lines_local = NULL;
+	long line_count = 0;
+
+	//if (NULL == bus_lines)
+	//{
+	//	goto cleanup;
+	//}
+
+	status = get_user_input(
+		LINE_COUNT_PROMPT, bus_line_count_tester, &line_count);
+	if (STATUS_FAILED(status))
+	{
+		goto cleanup;
+	}
+
+
+	// Ownership transfer
+	*bus_lines = lines_local;
+	lines_local = NULL;
+
+	status = PROGRAM_STATUS_SUCCESS;
+
+cleanup:
+	FREE_MEMORY(lines_local);
+
+	return status;
+}
+
+// See documentation at declaration
+ProgramStatus resolve_command_type(char* in, pCommandType cmd)
+{
+	CommandType out_cmd = COMMAND_INVALID;
+
+	if (NULL == in)
+	{
+		return PROGRAM_STATUS_FAILED;
+	}
+
+	if (0 == strcmp(in, COMMAND_BY_DURATION_STR))
+	{
+		out_cmd = COMMAND_BY_DURATION;
+	}
+	else if (0 == strcmp(in, COMMAND_BY_DISTANCE_STR))
+	{
+		out_cmd = COMMAND_BY_DISTANCE;
+	}
+	else if (0 == strcmp(in, COMMAND_BY_NAME_STR))
+	{
+		out_cmd = COMMAND_BY_NAME;
+	}
+	else if (0 == strcmp(in, COMMAND_TEST_STR))
+	{
+		out_cmd = COMMAND_TEST;
+	}
+
+	*cmd = out_cmd;
+
+	return PROGRAM_STATUS_SUCCESS;
 }
 
 /**
@@ -19,20 +254,23 @@ BusLine create_line(char* name, int distance, int duration)
  */
 int main (int argc, char *argv[])
 {
-	char* name1 = "ahelloworld";
-	char* name2 = "bhellobworld";
-	char* name3 = "bhelloaworld";
-	char* name4 = "dhelloworld";
-	BusLine* a = (BusLine*)calloc(4, sizeof(BusLine));
-	BusLine line1 = create_line(name1, 1, 1);
-	BusLine line2 = create_line(name2, 1, 1);
-	BusLine line3 = create_line(name3, 1, 1);
-	BusLine line4 = create_line(name4, 1, 1);
-	memcpy(a, &line1, sizeof(*a));
-	memcpy(a+1, &line2, sizeof(*a));
-	memcpy(a+2, &line3, sizeof(*a));
-	memcpy(a+3, &line4, sizeof(*a));
-	bubble_sort(a, a + 3);
-	//quick_sort(a, a + 3, DISTANCE);
-	printf("stahp");
+	ProgramStatus status = PROGRAM_STATUS_FAILED;
+	CommandType cmd = COMMAND_INVALID;
+
+	if (ARGUMENT_MAX_ARGS != argc)
+	{
+		goto cleanup;
+	}
+
+	status = resolve_command_type(argv[ARGUMENT_COMMAND], &cmd);
+	if (STATUS_FAILED(status))
+	{
+		(void)fprintf(stdout, USAGE_PROMPT);
+		goto cleanup;
+	}
+
+	get_bus_lines_input(NULL);
+
+cleanup:
+	return status;
 }
