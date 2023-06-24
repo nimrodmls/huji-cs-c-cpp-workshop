2e9f4767b28fb6e59203b05fdbe27e681421efa3
Nimrod Mallis <nimrodmallis@gmail.com>
2023-06-24 21:51:42 +0300

    Updated the main for tests

--- ex5/ex5/temp_main.cpp
+++ ex5/ex5/temp_main.cpp
@@ -8,14 +8,15 @@ int wmain()
 	rank_map ranking(5, sp_movie_hash, sp_movie_equal);
 	User user_1("NimM", ranking, rsys);
 
-	user_1.add_movie_to_rs("Titanic", 1997, std::vector<double>(), 5);
-	user_1.add_movie_to_rs("Wanted", 2008, std::vector<double>(), 5);
-	user_1.add_movie_to_rs("Twilight", 2008, std::vector<double>(), 5);
-
-	auto a = rsys->get_movie("Titanic", 1997);
-	user_1.get_recommendation_by_content();
+	user_1.add_movie_to_rs("ForestGump", 1997, {1, 7, 7, 8}, 5);
+	user_1.add_movie_to_rs("StarWars", 2008, {3, 3, 4, 9}, 6);
+	user_1.add_movie_to_rs("Batman", 2008, {2, 6, 4, 8}, 1);
+	rsys->add_movie("Titanic", 2008, {7, 2, 9, 1});
+	//user_1.add_movie_to_rs("Twilight", 2008, std::vector<double>(), 5);
 
 	std::cout << *rsys;
+
+	std::cout << rsys->predict_movie_score(user_1, rsys->get_movie("Titanic", 2008), 2);
 
 	return 0;
 }
